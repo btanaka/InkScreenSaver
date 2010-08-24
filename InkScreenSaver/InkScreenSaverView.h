36c24ffc66286a9c3cee6e46805dfc49b89f03ce
Brian Tanaka <brian.tanaka@gmail.com>
2010-08-23 21:05:44 -0700

    initial

---
+++ InkScreenSaver/InkScreenSaverView.h
@@ -0,0 +1,17 @@
+//
+//  InkScreenSaverView.h
+//  InkScreenSaver
+//
+//  Created by Brian Tanaka on 8/23/10.
+//  Copyright (c) 2010, Brian Tanaka Consulting Services. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface InkScreenSaverView : ScreenSaverView 
+{
+
+}
+
+@end
---
+++ InkScreenSaver/InkScreenSaverView.m
@@ -0,0 +1,53 @@
+//
+//  InkScreenSaverView.m
+//  InkScreenSaver
+//
+//  Created by Brian Tanaka on 8/23/10.
+//  Copyright (c) 2010, Brian Tanaka Consulting Services. All rights reserved.
+//
+
+#import "InkScreenSaverView.h"
+
+
+@implementation InkScreenSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
